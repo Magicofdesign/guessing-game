d7f011169f7da5ee1635f7e4d0fd4402120a413f
Magicofdesign <magicofdesign@gmail.com>
2017-01-30 15:06:09 +0300

    All tests are ok

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	var newGuess = Math.ceil( (this.max - this.min) / 2 + this.min );
+    	return newGuess;
     }
 
     lower() {
-
+    	this.max = Math.ceil( (this.max - this.min) / 2 + this.min );
     }
 
     greater() {
-
-    }
+    	this.min = Math.ceil( (this.max - this.min) / 2 + this.min );
+    } 
 }
 
 module.exports = GuessingGame;
